62634a8dc612d6128a4e0ae7a42624e662605d35
Jon Lambson <jonlambson@gmail.com>
2018-01-14 18:28:00 -0700

    fixing typo

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
