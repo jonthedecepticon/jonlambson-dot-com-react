ad097e98914f870200912b1cf1a9b0b9b9b4c7b7
Jon Lambson <jonlambson@gmail.com>
2018-01-15 16:33:07 -0700

    added mongodb, setup user model

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
