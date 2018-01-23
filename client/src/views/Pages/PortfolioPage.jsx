754ff348fdac0b40f0394991c5babccc0e41f1df
Jon Lambson <jonlambson@gmail.com>
2018-01-23 16:18:49 -0700

    removing stuff

--- client/src/views/Pages/PortfolioPage.jsx
+++ client/src/views/Pages/PortfolioPage.jsx
@@ -3,26 +3,13 @@ import { FormGroup, FormControl } from 'react-bootstrap';
 
 import Button from 'elements/CustomButton/CustomButton.jsx';
 
-import avatar from 'assets/img/default-avatar.png';
 
 class PortfolioPage extends Component{
   render(){
     return (
       <form className="ng-untouched ng-pristine ng-valid">
         <div className="user-profile">
-          <div className="author">
-            <img alt="..." className="avatar" src={avatar} />
-          </div>
-          <h4>PortFolio Page</h4>
-          <FormGroup>
-            <FormControl
-              type="password"
-              placeholder="Enter Password"
-            />
-          </FormGroup>
-          <Button wd neutral round>
-            Unlock
-          </Button>
+          <h4>Coming soon...</h4>
         </div>
       </form>
     );
