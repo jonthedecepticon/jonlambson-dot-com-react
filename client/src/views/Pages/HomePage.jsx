2884e05cc87472860cb96c9711aa94439644bf02
Jon Lambson <jonlambson@gmail.com>
2018-02-05 11:42:10 -0700

    added typing effect

--- client/src/views/Pages/HomePage.jsx
+++ client/src/views/Pages/HomePage.jsx
@@ -2,10 +2,73 @@ import React, { Component } from 'react';
 
 class HomePage extends Component{
   render(){
+    var TxtRotate = function(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function() {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+      var that = this;
+      var delta = 300 - Math.random() * 100;
+
+      if (this.isDeleting) { delta /= 2; }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function() {
+        that.tick();
+      }, delta);
+    };
+
+    window.onload = function() {
+      var elements = document.getElementsByClassName('txt-rotate');
+      for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+      }
+      // INJECT CSS
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+      document.body.appendChild(css);
+    };
     return (
       <form className="ng-untouched ng-pristine ng-valid">
-        <div className="user-profile">
+        <div className="user-profile" style={{width: "400px", marginLeft: "-200px"}}>
           <h2>Welcome...</h2>
+          <h4>I am a student of&nbsp;
+            <span
+              className="txt-rotate"
+              data-period="2000"
+            data-rotate='[ "Life!", "Full Stack Development.", "IoT Development.", "Javascript.", "Ruby on Rails.", "Swift.", "Design." ]'>
+            </span>
+          </h4>
         </div>
       </form>
     );
